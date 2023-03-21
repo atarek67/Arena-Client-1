2d6ab0ba2466aa5b7c4a64a03e31a85fa2d26677
ahmedmedhat365 <110779544+ahmedmedhat365@users.noreply.github.com>
2023-03-21 22:38:30 +0200

    Update Login.jsx

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -95,8 +95,8 @@ export default function Login(props) {
 
           <div className="d-flex justify-content-center m-5">
             <button className="btn btn-outline-success col-lg-3 col-6">
-              {t("Login")}
-              {isLoading? "loadingggg": t("Login")}
+//               {t("Login")}
+              {isLoading? "loadingggg": t"Login"}
             </button>
           </div>
         </form>
