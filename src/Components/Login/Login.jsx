21322e441c9dcfd0aadf9f96929062cb467728cf
ahmedmedhat365 <110779544+ahmedmedhat365@users.noreply.github.com>
2023-03-22 00:53:09 +0200

    Update Login.jsx

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -47,6 +47,7 @@ export default function Login(props) {
       .post(`/users/login`, user)
       .catch((err) => {
         setShowError(true);
+         setIsLoading(false);
       });
 
     if (data.message === "Logged In Successfully") {
